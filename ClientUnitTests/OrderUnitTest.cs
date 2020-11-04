07ad385d68ba14d05e1fbff4af29bb30c2690958
Matej Hackl <matej.hackl@hotmail.com>
2020-11-04 13:28:58 +0100

    EVERYTHING2

---
+++ ClientUnitTests/OrderUnitTest.cs
@@ -0,0 +1,46 @@
+using System;
+using NUnit.Framework;
+using ClientFramework.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientUnitTests
+{
+    public class Tests
+    {
+        public Order order;
+        
+        [SetUp]
+        public void Setup()
+        {
+            order = new Order();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsNotNull(order.getOrderId());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsNotNull(order.getOrderState());
+        }
+
+        [Test]
+        public void Test3()
+        {
+            order.setOrderId(3);
+            Assert.AreEqual(order.getOrderId(), 3);
+        }
+        
+
+        [Test]
+        public void Test5()
+        {
+
+            order.setOrderState(3);
+            Assert.AreEqual(order.getOrderState(), 3);
+        }
+    }
+}
