3a5a3354a224863d9ab4a326e5b4b13ea464cd63
Matej Hackl <matej.hackl@hotmail.com>
2020-11-07 15:35:41 +0100

    ClientWebService added

--- ServerFramework/Services/ClientWebService.cs
+++ ServerFramework/Services/ClientWebService.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClientFramework.Data;
 
 namespace ServerFramework.Services
 {
     interface ClientWebService
     {
+        public Task ConfirmOrder(int id);
+        public Task ConfirmOrder(Order order);
     }
 }
--- ServerFramework/Services/ClientWebServiceImpl.cs
+++ ServerFramework/Services/ClientWebServiceImpl.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using ClientFramework.Data;
 
 namespace ServerFramework.Services
 {
     public class ClientWebServiceImpl : ClientWebService
     {
+        private string DataURI;
+        
+        public async Task ConfirmOrder(Order order)
+        {
+            int i = order.getOrderId();
+            await ConfirmOrder(i);
+        }
+
+        public async Task ConfirmOrder(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            string serialId = JsonSerializer.Serialize(id, id.GetType());
+            StringContent content = new StringContent(serialId);
+            await httpClient.PostAsync(DataURI, content);
+        }
     }
 }
