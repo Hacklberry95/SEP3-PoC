0f6f6ec4fac2d0d2d55d9f6cad1bc4e58bb001e8
Kristóf Lénárd <293110@via.dk>
2020-11-07 14:31:32 +0100

    Started ServerFramework

---
+++ ServerFramework/Services/ClientWebServiceImpl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerFramework.Services
+{
+    public class ClientWebServiceImpl : ClientWebService
+    {
+    }
+}
