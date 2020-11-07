97057f30812c005ce2edcd9493917d4881fee032
Kristóf Lénárd <293110@via.dk>
2020-11-07 14:34:42 +0100

    Copied Order class

---
+++ ServerFramework/Data/Order.cs
@@ -0,0 +1,40 @@
+﻿namespace ClientFramework.Data
+{
+    public class Order
+    {
+        private int orderId;
+        private int orderState;
+
+        public Order()
+        {
+            orderId = -1;
+            orderState = 0;
+        }
+
+        public Order(int orderId)
+        {
+            this.orderId = orderId;
+            orderState = 0;
+        }
+
+        public void setOrderState(int i)
+        {
+            orderState = i;
+        }
+
+        public int getOrderState()
+        {
+            return orderState;
+        }
+
+        public void setOrderId(int id)
+        {
+            orderId = id;
+        }
+
+        public int getOrderId()
+        {
+            return orderId;
+        }
+    }
+}
