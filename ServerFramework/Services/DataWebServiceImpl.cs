5e63c830811df32adfdbead11f376a905a2d1566
MasterLegoYoda <bahrani.ali.2001@gmail.com>
2020-11-10 20:51:57 +0100

    E

--- ServerFramework/Services/DataWebServiceImpl.cs
+++ ServerFramework/Services/DataWebServiceImpl.cs
@@ -9,13 +9,6 @@ namespace ServerFramework.Services
 {
     public class DataWebServiceImpl : DataWebService
     {
-        public async Task RefactorTheNameLater(int OID)
-        {
-            HttpClient ObjectName = new HttpClient();
-            //localhost
-            Uri uri = new Uri("");
-            await ObjectName.PostAsync(uri, new StringContent(JsonSerializer.Serialize(OID,OID.GetType())));
-            //idk what this is
-        }
+      
     }
 }
