3f93682633ae08073f4a6aa8a2e4dee1c423c291
Matej Hackl <matej.hackl@hotmail.com>
2020-11-22 15:45:03 +0100

    Maybe a working socket?

--- ClientFramework/Program.cs
+++ ClientFramework/Program.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ClientFramework.Data;
 
 namespace ClientFramework
 {
@@ -16,6 +20,9 @@ namespace ClientFramework
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            
+
+          
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
