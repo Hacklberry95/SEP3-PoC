69fe4520c76947c82bf072dc2d9111092ee4dbbf
markvincze <mark.vincze.hu@gmail.com>
2020-11-30 12:42:02 +0100

    Picker class

---
+++ ClientFramework/Authorization/AuthRoles/Picker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClientFramework.Authorization.AuthRoles
+{
+    public class Picker : Role
+    {
+        public static new List<string> auth;
+
+        public Picker()
+        {
+            auth.Add("");
+        }
+    }
+}
