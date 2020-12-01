a88ed3c3b3779fe51be2ee97ff581a5d8761c145
MasterLegoYoda <bahrani.ali.2001@gmail.com>
2020-12-01 12:43:06 +0100

    pee pee poo poo 2

--- ServerFramework/Services/SocketServiceImpl.cs
+++ ServerFramework/Services/SocketServiceImpl.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace ServerFramework.Services
 {
@@ -6,13 +9,51 @@ namespace ServerFramework.Services
     {
         public async Task<string> TransmitAndReturnResponse(string jsonifiedObject)
         {
-            return null;
-            //throw new System.NotImplementedException();
+            string toSend = jsonifiedObject;
+
+            IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("192.168.0.6"), 4343);
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(serverAddress);
+
+            // Sending
+            int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+            byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+            byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+            clientSocket.Send(toSendLenBytes);
+            clientSocket.Send(toSendBytes);
+
+            // Receiving
+            byte[] rcvLenBytes = new byte[4];
+            clientSocket.Receive(rcvLenBytes);
+            int rcvLen = System.BitConverter.ToInt32(rcvLenBytes, 0);
+            byte[] rcvBytes = new byte[rcvLen];
+            clientSocket.Receive(rcvBytes);
+            String rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
+
+            Console.WriteLine("Client received: " + rcv);
+
+            clientSocket.Close();
+            return rcv;
         }
 
         public Task JustTransmit(string jsonifiedObject)
         {
-            throw new System.NotImplementedException();
+            string toSend = jsonifiedObject;
+
+            IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("192.168.0.6"), 4343);
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(serverAddress);
+
+            // Sending
+            int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+            byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+            byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+            clientSocket.Send(toSendLenBytes);
+            clientSocket.Send(toSendBytes);
+            clientSocket.Close();
+            return null;
         }
     }
 }
