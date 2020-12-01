8aeb563d0047604f4671690fa6b5a73ba04d578a
MasterLegoYoda <bahrani.ali.2001@gmail.com>
2020-12-01 12:07:51 +0100

    pee pee poo poo

---
+++ ServerFramework/Services/SocketServiceImpl.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace ServerFramework.Services
+{
+    public class SocketServiceImpl : ISocketService
+    {
+        public async Task<string> TransmitAndReturnResponse(string jsonifiedObject)
+        {
+            return null;
+            //throw new System.NotImplementedException();
+        }
+
+        public Task JustTransmit(string jsonifiedObject)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
