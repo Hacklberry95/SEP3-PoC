40d286ff19d30f951ba813baca5e069e8f3484c4
MasterLegoYoda <bahrani.ali.2001@gmail.com>
2020-12-02 14:49:08 +0100

    monke

---
+++ DBFramework/src/main/java/DBServer/networking/ConnectionPool.java
@@ -0,0 +1,13 @@
+package DBServer.networking;
+import java.util.ArrayList;
+import java.util.List;
+public class ConnectionPool
+{
+    private List<SocketHandler> connections = new ArrayList<>();
+
+    public synchronized void addHandler(SocketHandler handler)
+    {
+        connections.add(handler);
+    }
+
+}
---
+++ DBFramework/src/main/java/DBServer/networking/Start.java
@@ -0,0 +1,5 @@
+package DBServer.networking;
+
+public class Start
+{
+}
