b8cd733b329ec6218e0a2bd338f98f80fa325b9a
MasterLegoYoda <bahrani.ali.2001@gmail.com>
2020-12-06 14:47:17 +0100

    fix the remove order method in the orderqueue class

---
+++ ServerFramework/Services/IOrderQueue.cs
@@ -0,0 +1,19 @@
+﻿namespace ServerFramework.Services
+{
+    public interface IOrderQueue
+    {
+        void Clear();
+        void RemoveOrder(int rderId);
+        //void Enqueue(Order order);
+        //or
+        void Enque(int OrderId);
+        void EnqueueHighpriority(int orderId);
+        //or
+        //void EnqueueHighpriority(Order order);
+        int Dequeue();
+        int Peek(int pos);
+        //int Contains(Order order);
+        //or
+        int Contains(int OrderId);
+    }
+}
---
+++ ServerFramework/Services/OrderQueue.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using ServerFramework.Data;
+
+namespace ServerFramework.Services
+{
+    public class OrderQueue : IOrderQueue
+    {
+        private List<int> nQueue;
+        private List<int> hQueue;
+
+        public OrderQueue()
+        {
+            nQueue = new List<int>();
+            hQueue = new List<int>();
+        }
+
+        public void Clear()
+        {
+            nQueue = new List<int>();
+            hQueue = new List<int>();
+        }
+
+        public void RemoveOrder(int orderId)
+        {
+            if (hQueue.Contains(orderId))
+            {
+                int dex = hQueue.IndexOf(orderId);
+            }
+        }
+
+        public void Enque(int orderId)
+        {
+            nQueue.Add(orderId);
+        }
+
+        public void EnqueueHighpriority(int orderId)
+        {
+            hQueue.Add(orderId);
+        }
+
+        public int Dequeue()
+        {
+            if (hQueue.Count>0)
+            {
+                int res = hQueue[0];
+                int[] eee = new int[hQueue.Count];
+                hQueue.CopyTo(eee,1);
+                List<int> tmp1 = new List<int>(eee);
+                hQueue = tmp1;
+                return res;
+            }
+            else
+            {
+                int res = nQueue[0];
+                int[] eee = new int[nQueue.Count];
+                nQueue.CopyTo(eee,1);
+                List<int> tmp1 = new List<int>(eee);
+                nQueue = tmp1;
+                return res; 
+            }
+        
+        }
+
+        public int Peek(int pos)
+        {
+            if (pos <= hQueue.Count-1)
+            {
+                return hQueue[pos];
+            }
+            else
+            {
+                return nQueue[pos-hQueue.Count];
+            }
+            
+        }
+
+        public int Contains(int OrderId)
+        {
+            if (hQueue.Contains(OrderId))
+            {
+                return hQueue.IndexOf(OrderId);
+            }
+            else if(nQueue.Contains(OrderId))
+            {
+                return nQueue.IndexOf(OrderId);
+            }
+        }
+    }
